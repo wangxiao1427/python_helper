df1a9a850e9e035f4c4cad95d569ea2109dc72bd
shaw_wg <wang.xiao@intellif.com>
2018-12-19 18:10:24 +0800

    修复引用

--- aios_part_uploader/aios_part_uploader/uploader.py
+++ aios_part_uploader/aios_part_uploader/uploader.py
@@ -1,4 +1,5 @@
 import os
+import json
 import random
 import string
 import requests
--- aios_part_uploader/setup.py
+++ aios_part_uploader/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages            #这个包没有的可以
 
 setup(
     name = "aios_part_uploader",      #这里是pip项目发布的名称
-    version = "0.0.5",  #版本号，数值大的会优先被pip
+    version = "0.0.6",  #版本号，数值大的会优先被pip
     keywords = ("aios", "part", "uploader"),
     description = "aios part uploader",
     long_description = "aios part uploader",
