98964eee29ff1d9874176107c837fe206cad8b60
shaw_wg <wang.xiao@intellif.com>
2018-12-28 18:52:45 +0800

    更新引入问题

--- aios_part_uploader/setup.py
+++ aios_part_uploader/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages            #这个包没有的可以
 
 setup(
     name = "aios_part_uploader",      #这里是pip项目发布的名称
-    version = "0.0.13",  #版本号，数值大的会优先被pip
+    version = "0.0.14",  #版本号，数值大的会优先被pip
     keywords = ("aios", "part", "uploader"),
     description = "aios part uploader",
     long_description = "aios part uploader",
